db971d025000f24389fc4ee9f1fbc846d15c7516
Skyler Xiao <skyler.xiao.99@gmail.com>
2026-07-28 16:44:23 -0400

    Stage 1: signup and login routes working

--- app.js
+++ app.js
@@ -18,10 +18,8 @@ const pool = new Pool({
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Something went wrong.' });
-});
+
+
 
 app.get('/', (req, res) => {
   res.json({"name": "Task API", "version": "1.0", "endpoint": ["/tasks"]});
@@ -83,6 +81,44 @@ app.delete('/tasks/:id', async (req, res) => {
 
     const result = await pool.query('DELETE FROM tasks WHERE id = $1', [taskId]);
     res.status(204).send();
+});
+
+app.post('/auth/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and/or password are required' });
+    }
+
+    const { data, error } = await supabase.auth.signUp({ email, password });
+    if (error) {
+        return res.status(400).json({ error: error.message });
+    }
+    return res.status(201).json({message: 'Created', user: data.user});
+});
+
+
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and/or password are required' });
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+        return res.status(401).json({ error: 'Invalid login credentials' });
+    }
+    return res.status(200).json({ 
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token
+     });
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong.' });
 });
 
 app.listen(port, () => {
